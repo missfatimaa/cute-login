637a0025de652a165721cb93f895caf5607a0967
missfatimaa <fatemehjaafari3672@gmail.com>
2024-07-12 23:01:14 +0430

    added the events of changing src

--- java.js
+++ java.js
@@ -1,5 +1,12 @@
-// let $ = document
-// let input = $.querySelectorAll("form input")
 
+let userInput = document.querySelector("#username")
+let passInput = document.querySelector("#password")
+let girrafeImage = document.querySelector("#girrafe-image")
 
+$(passInput).focus(function (e) {
+    girrafeImage.src = "/image/girrafe2.png"
+});
 
+$(userInput).focus(function (e) {
+    girrafeImage.src = "/image/girrafe.png"
+});
