319eefd6ca64ba10b4291a61093d029c9f5031a3
missfatimaa <fatemehjaafari3672@gmail.com>
2024-07-17 02:52:30 +0430

    code the hooves

--- java.js
+++ java.js
@@ -3,9 +3,10 @@ let userInput = document.querySelector("#username")
 let passInput = document.querySelector("#password")
 let girrafeImage = document.querySelector("#girrafe-image")
 
-// $(passInput).focus(function (e) {
-//     girrafeImage.src = "/image/girrafe2.png"
-// });
+$(passInput).focus(function (e) {
+    // girrafeImage.src = "/image/girrafe2.png"
+    
+});
 
 // $(userInput).focus(function (e) {
 //     girrafeImage.src = "/image/girrafe.png"
